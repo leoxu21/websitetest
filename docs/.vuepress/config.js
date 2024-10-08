c7c238c1d8f0d969f5c28221150da28d26f6f875
leoxu21 <102285342+leoxu21@users.noreply.github.com>
2024-10-08 22:02:27 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: "/websitetest/",
   lang: 'en-US',
 
   title: 'VuePress',
